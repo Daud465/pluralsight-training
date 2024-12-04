1a725ac08f76046eb2deb4f82cc30354bf0cc66d
dawoodarshadsahootra <dawoodarshadsahootra@gmail.com>
2024-12-04 16:07:23 +0000

    hello again

---
+++ cpp/hello.c
@@ -0,0 +1,8 @@
+//This program displays “Hello World!” on the screen
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!\n";
+    return 0;
+}
--- exercises/maths.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(void)
-{
-    int n1 = 5;
-    int n2 = 9;
-
-    std::cout << n1+n2 << '\n';
-}
